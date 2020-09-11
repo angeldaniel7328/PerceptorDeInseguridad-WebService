33f6940bca88ee806b0002d4150b8dbd7f0fd5f5
Alan de Jesus Ramirez Valencia <48893881+AlanDeJesusRamirezValencia@users.noreply.github.com>
2020-09-11 12:38:11 -0500

    Update PerceptionHistory.php
    Ahora, cuando el historial esté vacío, el servicio retorará un arreglo vacío.
    Y cuando el historial contenga uno o más registros, el servicio retornará un arreglo con el nombre: datos.
    
    La consulta se redujo. Ahora los datos UTM no son consultados.

--- appBotonDePanico/PerceptionsHistory.php
+++ appBotonDePanico/PerceptionsHistory.php
@@ -5,19 +5,27 @@
 		Test user null: http://www.edacarquitectos.com/appBotonDePanico/PerceptionsHistory.php?id_usuario=2
 	*/
 	include "conexion.php";
+
+	$datosVacios["id_percepcion"]='';
+    $datosVacios["latitud"]='';
+    $datosVacios["longitud"]='';
+    $datosVacios["valor_inseguridad"]='';
+    $datosVacios["tipo_peligro"]='';
+    $jsonVacio['datos'][]=$datosVacios;
+
 	$id_usuario = $_GET["id_usuario"];
 	
-	$sql = "SELECT p.id_percepcion, p.latitud, p.longitud, p.fecha_hora, p.valor_inseguridad, p.x, p.y, p.zona, p.hemisferio, p.contexto, p.tipo_peligro FROM usuario u INNER JOIN percepcion p ON u.id_usuario = p.fk_usuario WHERE u.id_usuario = $id_usuario;";
+	$sql = "SELECT p.id_percepcion, p.latitud, p.longitud, p.valor_inseguridad, p.tipo_peligro FROM usuario u INNER JOIN percepcion p ON u.id_usuario = p.fk_usuario WHERE u.id_usuario = $id_usuario;";
 	
 	$query = mysqli_query($conexion, $sql);
 	
 	
 	while($registros = mysqli_fetch_array($query)){
-		$arreglo[] = array_map("utf8_encode", $registros);
+		$arreglo["datos"][] = array_map("utf8_encode", $registros);
 	}
 	
 	if($arreglo == null){
-		echo "SIN HISTORIAL";
+		echo json_encode($jsonVacio);
 	}else{
 		echo json_encode($arreglo);
 	}
